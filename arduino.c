6faea9b115ff4a8448b938a6f7157412f1ec987d
Carson101504 <156962652+Carson101504@users.noreply.github.com>
2026-09-08 17:09:14 -0400

    Create arduino.c

---
+++ arduino.c
@@ -0,0 +1,18 @@
+// EE Capstone Design
+// CPR - Alarm Clock
+// Authors - Cade Young, Dylan Hale, Carson Mackey, Andrew Harp
+
+// Define Constants
+const int backButton = 4;
+const int homeButton = 5;
+const int snoozeButton = 6;
+const int resetButton = 7;
+
+void setup(){
+  Serial.begin(115200); // enable serial comms
+  Serial.println("ESP32-S3 working!"); // serial monitor test output
+}
+
+void loop(){
+  // do nothing
+}
